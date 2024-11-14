50ad74641237ce402716b58f2f1884838fd8c014
CCauanf <161883786+CCauanf@users.noreply.github.com>
2024-11-14 20:46:42 -0300

    Create Ex5Cap4.c

---
+++ Programacao/Fundamentos da Programação de Computadores/Cap 4/Ex5Cap4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(){
+    float num1, num2, operacao;
+    int resposta;
+
+    printf("Digite dois numeros: ");
+    scanf("%f %f", &num1, &num2);
+
+    printf("Digite a operacao que deseja realizar:\n");
+    printf("1 - Media entre os numeros digitados\n");
+    printf("2 - Diferenca do maior pelo menor\n");
+    printf("3 - Produto entre os numeros digitados\n");
+    printf("4 - Divisao do primeiro pelo segundo\n");
+    scanf("%i", &resposta);
+    printf("Resposta: ") ;
+    
+    switch(resposta){
+        case 1: 
+            operacao = (num1 + num2) / 2;
+            break;
+        case 2:
+            if(num1 > num2) operacao = num1 - num2; 
+            else operacao = num2 - num1;
+            break;
+        case 3:
+            operacao = num1 * num2;
+            break;
+        case 4:
+            operacao = num1 / num2;
+            break;
+        default: 
+            printf("Digite uma opção valida!");
+            return 0;
+    }
+
+    printf("%.2f", operacao);
+
+    return 0;
+}
